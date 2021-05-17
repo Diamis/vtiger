cd46d64a39b48f3349c79ebb7ea1d9a9f26ac391
Aleksey Chernyshov <alexey.chernyshov@distate.ru>
2021-05-17 13:24:19 +0300

    first commit

---
+++ test/templates_c/v7/6c37e8933eb56c104511cd3598a32724fdba74b4.file.InstallPreProcess.tpl.php
@@ -0,0 +1,41 @@
+<?php /* Smarty version Smarty-3.1.7, created on 2021-05-17 09:02:14
+         compiled from "/var/www/html/includes/runtime/../../layouts/v7/modules/Install/InstallPreProcess.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:118392007160a22306e569c0-39461792%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6c37e8933eb56c104511cd3598a32724fdba74b4' => 
+    array (
+      0 => '/var/www/html/includes/runtime/../../layouts/v7/modules/Install/InstallPreProcess.tpl',
+      1 => 1602587794,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '118392007160a22306e569c0-39461792',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.7',
+  'unifunc' => 'content_60a22306e6fea',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_60a22306e6fea')) {function content_60a22306e6fea($_smarty_tpl) {?>
+
+<input type="hidden" id="module" value="Install" />
+<input type="hidden" id="view" value="Index" />
+<div class="container-fluid page-container">
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="logo">
+				<img src="<?php echo vimage_path('logo.png');?>
+"/>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="head pull-right">
+				<h3><?php echo vtranslate('LBL_INSTALLATION_WIZARD','Install');?>
+</h3>
+			</div>
+		</div>
+	</div>
+<?php }} ?>
